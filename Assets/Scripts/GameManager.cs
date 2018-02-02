bf6e6cfcc9f8a1e64f0fda1378d0040e15bfde8f
DESKTOP-3RI9R03\ido <games@ido.wtf>
2018-02-02 17:55:06 +0200

    button bring chat to bottom

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public ScrollRect s;
+    //public RectTransform chatScroll;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChatToEnd()
+    {
+        s.normalizedPosition = Vector2.zero;
+        //chatScroll.localPosition =  new Vector2(chatScroll.localPosition.x, chatScroll.sizeDelta.y);
+    }
+}
