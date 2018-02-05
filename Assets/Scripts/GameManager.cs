b0b96e0b17e85cfbd610de2be9637e09638b1cb5
DESKTOP-3RI9R03\ido <games@ido.wtf>
2018-02-05 14:57:20 +0200

    experiments with keyboroard panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,48 @@
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-    public ScrollRect s;
-    //public RectTransform chatScroll;
+    public RectTransform content;
+    public ScrollRect chatBack;
+    public InputField input;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+       //         content.offsetMin = new Vector2(content.offsetMin.x, GetKeyboardSize());// TouchScreenKeyboard.area.height);
+        /*if (TouchScreenKeyboard.visible)
+        {
+            if (GetKeyboardSize() > 0)
+            {
+                input.text = "" + GetKeyboardSize(); // TouchScreenKeyboard.area.height + "," + TouchScreenKeyboard.area.max.y + "," + TouchScreenKeyboard.area.size.y + "," + TouchScreenKeyboard.area.yMax + "," + TouchScreenKeyboard.area.y;
+            }
+        } else
+        {
+            content.offsetMin = new Vector2(content.offsetMin.x, 0);
+        }*/
+    }
 
     public void ChatToEnd()
     {
-        s.normalizedPosition = Vector2.zero;
-        //chatScroll.localPosition =  new Vector2(chatScroll.localPosition.x, chatScroll.sizeDelta.y);
+        chatBack.normalizedPosition = Vector2.zero;
+        
+    }
+
+    public int GetKeyboardSize()
+    {
+        using (AndroidJavaClass UnityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            AndroidJavaObject View = UnityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer").Call<AndroidJavaObject>("getView");
+
+            using (AndroidJavaObject Rct = new AndroidJavaObject("android.graphics.Rect"))
+            {
+                View.Call("getWindowVisibleDisplayFrame", Rct);
+
+                return Screen.height - Rct.Call<int>("height");
+            }
+        }
     }
 }
---
+++ Assets/Scripts/InputFieldForScreenKeyboardPanelAdjuster.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldForScreenKeyboardPanelAdjuster : MonoBehaviour
+{
+
+    // Assign panel here in order to adjust its height when TouchScreenKeyboard is shown
+    public GameObject panel;
+
+    private InputField inputField;
+    private RectTransform panelRectTrans;
+    private Vector2 panelOffsetMinOriginal;
+    private float panelHeightOriginal;
+    private float currentKeyboardHeightRatio;
+
+    public void Start()
+    {
+        TouchScreenKeyboard.hideInput = true;
+        inputField = transform.GetComponent<InputField>();
+        panelRectTrans = panel.GetComponent<RectTransform>();
+        panelOffsetMinOriginal = panelRectTrans.offsetMin;
+        panelHeightOriginal = panelRectTrans.rect.height;
+    }
+
+    public void LateUpdate()
+    {
+        TouchScreenKeyboard.hideInput = true;
+        if (inputField.isFocused)
+        {
+            float newKeyboardHeightRatio = GetKeyboardHeightRatio();
+            if (currentKeyboardHeightRatio != newKeyboardHeightRatio)
+            {
+                Debug.Log("InputFieldForScreenKeyboardPanelAdjuster: Adjust to keyboard height ratio: " + newKeyboardHeightRatio);
+                currentKeyboardHeightRatio = newKeyboardHeightRatio;
+                panelRectTrans.offsetMin = new Vector2(panelOffsetMinOriginal.x, panelHeightOriginal * currentKeyboardHeightRatio);
+            }
+        }
+        else if (currentKeyboardHeightRatio != 0f)
+        {
+            if (panelRectTrans.offsetMin != panelOffsetMinOriginal)
+            {
+                StartCoroutine( DelayedExecute(() => {
+                    Debug.Log("InputFieldForScreenKeyboardPanelAdjuster: Revert to original");
+                    panelRectTrans.offsetMin = panelOffsetMinOriginal;
+                }, 0.5f));
+            }
+            currentKeyboardHeightRatio = 0f;
+        }
+    }
+
+    private IEnumerator DelayedExecute(Action f, float t)
+    {
+        yield return new WaitForSeconds(t);
+        f();
+    }
+
+    private float GetKeyboardHeightRatio()
+    {
+        if (Application.isEditor)
+        {
+            return 0.4f; // fake TouchScreenKeyboard height ratio for debug in editor        
+        }
+
+#if UNITY_ANDROID        
+        using (AndroidJavaClass UnityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            AndroidJavaObject View = UnityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer").Call<AndroidJavaObject>("getView");
+            using (AndroidJavaObject rect = new AndroidJavaObject("android.graphics.Rect"))
+            {
+                View.Call("getWindowVisibleDisplayFrame", rect);
+                return (float)(Screen.height - rect.Call<int>("height")) / Screen.height;
+            }
+        }
+#else
+        return (float)TouchScreenKeyboard.area.height / Screen.height;
+#endif
+    }
+
+}
